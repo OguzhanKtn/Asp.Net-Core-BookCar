a30b291f27aaa92810a00f450ab66fc1bec328ec
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2024-07-21 13:52:58 +0300

    Entities

---
+++ BookCar/Core/Domain/Entities/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public int BrandID { get; set; }
+        public Brand Brand { get; set; }
+        public string Model { get; set; }
+    }
+}
