3cd345ee15ec79bb4417923feb301964f42cc429
Oğuzhan <81297977+OguzhanKtn@users.noreply.github.com>
2024-07-22 23:15:31 +0300

    Entitites

--- BookCar/Core/Domain/Entities/Car.cs
+++ BookCar/Core/Domain/Entities/Car.cs
@@ -12,5 +12,14 @@ namespace Domain.Entities
         public int BrandID { get; set; }
         public Brand Brand { get; set; }
         public string Model { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int Km { get; set; }
+        public string Transmission { get; set; }
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public string Fuel { get; set; }
+        public string BigImageUrl { get; set; }
+        public List<CarFeature> CarFeatures { get; set; }
+        public List<CarDescription> CarDescriptions { get; set; }
     }
 }
---
+++ BookCar/Core/Domain/Entities/Testimonial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
