ede8cb9121d18d5a8cf4446a61f4ff8b4779d90e
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2024-07-24 10:39:19 +0300

    EntitiesCompleted

--- BookCar/Core/Domain/Entities/Car.cs
+++ BookCar/Core/Domain/Entities/Car.cs
@@ -21,5 +21,6 @@ namespace Domain.Entities
         public string BigImageUrl { get; set; }
         public List<CarFeature> CarFeatures { get; set; }
         public List<CarDescription> CarDescriptions { get; set; }
+        public List<CarPricing> CarPricings { get; set; }
     }
 }
