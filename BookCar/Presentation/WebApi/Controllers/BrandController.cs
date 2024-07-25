35a5d00661add40c18e007501b10399f8ffb6cf2
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2024-07-25 17:54:12 +0300

    BrandApi

---
+++ BookCar/Presentation/WebApi/Controllers/BrandController.cs
@@ -0,0 +1,52 @@
+﻿using Application.Features.CQRS.Commands.BrandCommands;
+using Application.Features.CQRS.Queries.BrandQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BrandController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BrandList()
+        {
+            var values = await _mediator.Send(new GetBrandQuery());
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBrand(int id)
+        {
+            var value = await _mediator.Send(new GetBrandByIdQuery(id));
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBrand(CreateBrandCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Başarıyla Eklendi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateBrand(UpdateBrandCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Başarıyla Güncellendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+            await _mediator.Send(new RemoveBrandCommand(id));
+            return Ok("Başarıyla Silindi");
+        }
+    }
+}
