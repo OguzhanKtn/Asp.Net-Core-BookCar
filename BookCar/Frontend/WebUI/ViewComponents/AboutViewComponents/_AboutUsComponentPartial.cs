68d272a208525947f4bf10c3f80694bf0ddc4dd1
Oğuzhan <81297977+OguzhanKtn@users.noreply.github.com>
2024-07-30 22:57:08 +0300

    AboutApiConsume

--- BookCar/Frontend/WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ BookCar/Frontend/WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebUI.ViewComponents.AboutViewComponents
 {
@@ -14,7 +16,13 @@ namespace WebUI.ViewComponents.AboutViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var response = await client.GetAsync("");
+            var response = await client.GetAsync("'https://localhost:44380/api/About");
+            if(response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<IEnumerable<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
